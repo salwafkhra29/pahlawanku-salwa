93f047fac7c2b6d2000545c5387726369db5ee53
salwafkhra29 <112602374+salwafkhra29@users.noreply.github.com>
2022-11-16 16:24:45 +0700

    update adaptercard

--- app/src/main/java/com/si5a/pahlawanku_s/AdapterCard.java
+++ app/src/main/java/com/si5a/pahlawanku_s/AdapterCard.java
@@ -1,4 +1,50 @@
 package com.si5a.pahlawanku_s;
 
-public class AdapterCard {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder>{
+    private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ClassViewHolder extends RecyclerView.ViewHolder{
+        TextView tvNama,tvTentang;
+        ImageView ivFoto;
+
+        public ClassViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvTentang = itemView.findViewById(R.id.tv_tentang);
+            ivFoto = itemView.findViewById(R.id.iv_foto);
+
+        }
+    }
 }
