c4459561a46d80b6d1ccca3fbd91c09d98babeaa
salwa <112602374+salwafkhra29@users.noreply.github.com>
2022-11-23 16:42:44 +0700

    coba_coba

--- app/src/main/java/com/si5a/pahlawanku_s/MainActivity.java
+++ app/src/main/java/com/si5a/pahlawanku_s/MainActivity.java
@@ -1,23 +1,28 @@
 package com.si5a.pahlawanku_s;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvPahlawan;
     private ArrayList<ModelPahlawan> data = new ArrayList<>();
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         rvPahlawan = findViewById(R.id.rv_pahlawan);
         rvPahlawan.setHasFixedSize(true);
 
@@ -26,10 +31,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void tampilDataCard(){
+    private void tampilDataCard() {
         rvPahlawan.setLayoutManager(new LinearLayoutManager(this));
-        AdapterCard AC = new AdapterCard(data,MainActivity.this);
+        AdapterCard AC = new AdapterCard(data, MainActivity.this);
         rvPahlawan.setAdapter(AC);
 
+    }
+
+    private void tampilDataGrid() {
+        rvPahlawan.setLayoutManager(new GridLayoutManager(this, 2));
+        AdapterGrid AG = new AdapterGrid(data, MainActivity.this);
+        rvPahlawan.setAdapter(AG);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tampilan,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_grid:
+                tampilDataGrid();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
